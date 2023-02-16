14a6d8b9303e6d4bcc7027a588cca033774c9b07
Ayushjaisgit <ayushcodez@gmail.com>
2023-02-16 14:23:29 +0530

    encyption and hashing implementation

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const User = require('../models/User')
 // express validator for the valid data sending 
 const { body, validationResult } = require('express-validator');
+// bcrypt used for hashing salt and pepper
+const bcrypt = require('bcryptjs');
 
 
 
 
 // create the user using POST "/api/auth/createuser" does not require auth no login required
 // used to send data to the DB
+
 router.post('/createuser',[   
     body('name', ' Enter a valid name').isLength({min:3}),
 
@@ -19,8 +22,10 @@ router.post('/createuser',[
     body('password',' Password must be min 5').isLength({ min: 5 })
 
 ],async(req,res)=>{
+
     // if there are errors return bad error
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
@@ -29,16 +34,24 @@ router.post('/createuser',[
     // check whether the user with this email exists already
     try{
 
-    
+    // create a new user 1
     let user = await User.findOne({email:req.body.email})
+
     console.log(user)
+
     if(user){
         return res.status(400).json({error:'sorry a user with this email already exists'})
     }
+
+
+    // using bcryptjs 
+    const salt = await bcrypt.genSalt(10) // generating random salt of ten values
+    const secPass = await bcrypt.hash( req.body.password, salt)
+
     user = await User.create({
         name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+        email:req.body.email ,
+        password:secPass ,
       })
 
     //   .then(user => res.json(user))
@@ -52,13 +65,7 @@ router.post('/createuser',[
 
 
     })
-
-
-
-
-
-
-
+ 
 // sending data from body to DB 
     // const user = User(req.body)
     // user.save()
