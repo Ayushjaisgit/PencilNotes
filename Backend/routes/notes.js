b8f31a6e14858a5c8f55f7f9984abdf3d557c98c
Ayushjaisgit <ayushcodez@gmail.com>
2023-02-22 20:24:13 +0530

    update endpoint added

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -10,8 +10,14 @@ const fetchuser = require('../middleware/FetchUser')
 // Route 1  getting all the notes using GET:"/api/auth/getuser" . login required
 
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
-    const notes = await Notes.find({ user: req.user.id });
-    res.json(notes)
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes)
+    } catch (error) {
+        console.log(error.messege);
+        res.status(500).send("some error occured")
+    }
+
 })
 
 
@@ -25,27 +31,55 @@ router.post('/addnote', fetchuser, [
     body('title', ' Enter a valid title').isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
 
-    try {
+        try {
 
-        const { title, description, tag, } = req.body
-        // if there are errors return bad request and the error
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            const { title, description, tag, } = req.body
+            // if there are errors return bad request and the error
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            // add a new note 
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+            res.json(savedNote)
         }
-        // add a new note 
-        const note = new Note({
-            title, description, tag, user: req.user.id
+
+        catch (error) {
+            console.log(error.messege);
+            res.status(500).send("some error occured")
+        }
+
+    })
+
+        // Route 3  Update an existing notes using POST:"/api/notes/updatenote" . login required
+
+        router.put('/updatenote/:id', fetchuser, async (req, res) => {
+            const { title, description, tag, } = req.body
+
+            // Create a newNote object
+            const newNote = {};
+            if (title) { newNote.title = title };
+            if (description) { newNote.description = description };
+            if (tag) { newNote.tag = tag };
+
+            // Find the note to be updated and update it 
+            let note = await Note.findById(req.params.id);
+            if (!note) { return res.status(404).send("Not Found")}
+
+            if (note.user.toString() !== req.user.id) {
+                return res.status(401).send("Not Allowed");
+            }
+
+            note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+            res.json({note});
+
+
+
         })
-        const savedNote = await note.save()
-        res.json(savedNote)
-    }
 
-    catch (error) {
-        console.log(error.messege);
-        res.status(500).send("some error occured")
-    }
 
-    //   const notes = await Notes.find({user: req.user.id});
-})
+ 
 module.exports = router;
