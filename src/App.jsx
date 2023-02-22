0c5a59ddd99749684120a2141ec8dc9796469bc7
Ayushjaisgit <ayushcodez@gmail.com>
2023-02-22 21:19:56 +0530

    use of Concurrently for start of backend server and frontend server togther with npm run both

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <header className="Append" style={{textAlign: "center"}}>
+      <h1>CRUD note taking app</h1>
+    </header>
+  );
+}
+
+export default App;
