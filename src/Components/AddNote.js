d831ef7d5d9450b71aa5827f33560345fe9f53ba
Ayushjaisgit <ayushcodez@gmail.com>
2023-02-24 01:34:07 +0530

    added CRUD features

--- src/Components/AddNote.js
+++ src/Components/AddNote.js
@@ -3,14 +3,15 @@ import noteContext from "../context/notes/noteContext"
 import { useState } from "react"
 
 const AddNote = () => {
-    
+
     const context = useContext(noteContext);
     const {addNote} = context
   
-    const [note, setnote] = useState({title:"", description:"",tag:""})
+    const [note, setnote] = useState({title:"", description:"",tag:"default"})
 
-    const handleClick=()=>{
-        addNote(note)
+    const handleClick=(e)=>{
+        e.preventDefault()
+        addNote(note.title, note.description, note.tag,)
     }
     const onChange=(e)=>{
         setnote({...note, [e.target.name]: [e.target.value]})
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,108 +1,105 @@
 import { useState } from 'react'
 import noteContext from "./noteContext"
 
-const NoteState = (props) =>{
-    const notesInitial = [
-        {
-          "_id": "63f5521eccd4af8ac7fdd99ba16",
-          "user": "63efea77887af454795a2cb3",
-          "title": "My title",
-          "description": "Please Wake up and Early",
-          "tag": "Personal",
-          "date": "2023-02-21T23:22:06.205Z",
-          "__v": 0
-        },
-        
-        {
-          "_id": "63f55353d3e7bdfddbad3ad846be",
-          "user": "63efea77887af454795a2cb3",
-          "title": "My title",
-          "description": "Please Wake up and Early",
-          "tag": "Personal",
-          "date": "2023-02-21T23:27:15.315Z",
-          "__v": 0
-        },
-        {
-          "_id": "63f55353d3e7bbadsd3ad846c0",
-          "user": "63efea77887af454795a2cb3",
-          "title": "My title",
-          "description": "Please Wake up and Early",
-          "tag": "Personal",
-          "date": "2023-02-21T23:27:15.526Z",
-          "__v": 0
-        }
-    ]
-
-    
-    const [notes, setNotes] = useState(notesInitial)
-
-    // Add note function
-    const addNote =(title, description, tag)=>{
-      // TODO = API CALL
-     const note = {
-        "_id": "63f55353d3e7bbadsd3ad846c0",
-          "user": "63efea77887af454795a2cb3",
-          "title": "My title",
-          "description": "Please Wake up and Early",
-          "tag": "Personal",
-          "date": "2023-02-21T23:27:15.526Z",
-          "__v": 0
-      }; 
-      setNotes(notes.push(note))
-    }
-
-    // Delete note function
-    const deleteNote =(id)=>{
-
-    }
-    // Edit note function
-    const editNote =()=>{
-
-    }
-
+const NoteState = (props) => {
+  const host = "http://localhost:5000"
+  const notesInitial =[]
   
 
-    return(
-        <noteContext.Provider value={{notes, addNote, deleteNote, editNote}}>
-            {props.children}
-        </noteContext.Provider>
-    )
+
+  const [notes, setNotes] = useState(notesInitial)
+
+  // Get all notes function
+  const getNotes = async () => {
+    // API CALL
+    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNlZmVhNzc4ODdhZjQ1NDc5NWEyY2IzIn0sImlhdCI6MTY3NzAxODE1Nn0.CI6o8sfJeAd5l1eJ7Q9VEW5G55vDW2oYqmrYC0RdhFc",
+        'Content-Type': 'application/json'
+
+      },
+    });
+    const json = await response.json()
+    console.log(json)
+    setNotes(json)
+    // add note logic
+
+
+  }
+  // Add note function
+  const addNote = async (title, description, tag) => {
+    console.log("adding a new note")
+    // API CALL
+    const response = await fetch(`${host}/api/notes/addnote`, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+
+      headers: {
+        'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNlZmVhNzc4ODdhZjQ1NDc5NWEyY2IzIn0sImlhdCI6MTY3NzAxODE1Nn0.CI6o8sfJeAd5l1eJ7Q9VEW5G55vDW2oYqmrYC0RdhFc",
+        'Content-Type': 'application/json'
+
+      },
+      body: JSON.stringify({title, description, tag})
+    });
+    
+
+    // add note logic
+
+
+    const note = {
+      "_id": "fdsdff",
+      "user": "63efea77887af454795a2cb3",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2023-02-21T23:27:15.526Z",
+      "__v": 0
+    };
+    setNotes(notes.concat(note))
+  }
+
+  // Delete note function
+  const deleteNote = (id) => {
+    // TODO API CALL
+    console.log("Deleting the note with id" + id)
+    const newNotes = notes.filter((note) => { return note._id !== id })
+    setNotes(newNotes)
+  }
+  // Edit note function
+  const editNote = async (id, title, description, tag) => {
+    // api call 
+
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+
+      headers: {
+        'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNlZmVhNzc4ODdhZjQ1NDc5NWEyY2IzIn0sImlhdCI6MTY3NzAxODE1Nn0.CI6o8sfJeAd5l1eJ7Q9VEW5G55vDW2oYqmrYC0RdhFc",
+        'Content-Type': 'application/json'
+
+      },
+      body: JSON.stringify({title, description, tag})
+    });
+    const json = response.json(); // parses JSON response into native JavaScript objects
+
+    // logic to edit in client
+    for (let index = 0; index < notes.length; index++) {
+      const element = notes[index];
+      if (element._id === id) {
+        element.title = title;
+        element.description = description;
+        element.tag = tag;
+      }
+
+    }
+  }
+
+
+
+  return (
+    <noteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
+      {props.children}
+    </noteContext.Provider>
+  )
 }
 
 export default NoteState
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// it was a demonstration for the sake of the understanding of Context API
-// const s1={
-    //     "name":"Ayush",
-    //     "class":"6th"
-    // }
-    
-    // const [state, setState] = useState(s1)
-    // const update=() =>{
-        //     setTimeout(() => {
-            //         setState({
-                //             "name":"lary",
-                //             "class":"10th"
-//         })
-//     }, 1000);
-// }
-
-
-
-
-{/* <noteContext.Provider value={{state, update}}> */}
